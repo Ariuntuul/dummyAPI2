4720b53d21419cd6172bc5097026e6f63754b53a
Ariuntuul <ariuntuulenkhtaivan@gmail.com>
2022-07-08 12:27:45 -0500

    testPeople- corrected a spelling

--- src/test/java/clover/tests/testPeople.java
+++ src/test/java/clover/tests/testPeople.java
@@ -4,7 +4,6 @@ import clover.testBase.ghibliTestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import static io.restassured.RestAssured.given;
@@ -15,11 +14,11 @@ public class testPeople extends ghibliTestBase {
     public void test01_printAllPeople(){
 
         Response response = given().accept(ContentType.JSON)
-                .when().get("/People");
+                .when().get("/people");
 
         System.out.println("Status code: " + response.statusCode());
         if (response.statusCode() == 200){
-            System.out.println("Succesfull response");
+            System.out.println("Successful response");
         } else {
             System.out.println("Error!!!");
         }
@@ -35,11 +34,12 @@ public class testPeople extends ghibliTestBase {
 
         Response response = given().accept(ContentType.JSON)
                 .and().pathParam("id", "fe93adf2-2f3a-4ec4-9f68-5422f1b87c01")
-                .when().get("/People/{id}");
+                .when().get("/people/{id}");
 
         //System.out.println("Status code: " + response.statusCode());
+
         if (response.statusCode() == 200){
-            System.out.println("Succesfull response");
+            System.out.println("Successful response");
         } else {
             System.out.println("Error!!!");
         }
@@ -54,16 +54,17 @@ public class testPeople extends ghibliTestBase {
     public void test03_printListOfPerson(){
 
         Response response = given().accept(ContentType.JSON)
-                .when().get("/People");
+                .when().get("/people");
 
         if (response.statusCode() == 200){
-            System.out.println("Succesfull response");
+            System.out.println("Successful response");
         } else {
             System.out.println("Error!!!");
         }
 
+        //All data are inside the Array, therefore I created a list, then
         List<String> allPeople = response.path("name");
-        System.out.println("Number of people: " + allPeople.size());
+        System.out.println("Total number of data: " + allPeople.size());
 
         for (int i = 0; i < allPeople.size(); i++){
 
@@ -71,11 +72,13 @@ public class testPeople extends ghibliTestBase {
 
         }
 
-        System.out.println(allPeople.contains("Thomas"));
+        //System.out.println(allPeople.contains("Thomas"));
 
         Assertions.assertTrue(allPeople.contains("Thomas"));
 
     }
+
+
 
 
 
